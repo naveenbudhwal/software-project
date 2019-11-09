ad8e5198ce37b014860ed92859aa7020eb9302a6
naveenbudhwal <naveenbudhwal@gmail.com>
2019-11-09 23:18:48 +0530

    Added menu page

---
+++ Client/src/views/Menu.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Menu</h1>
+    <hr>
+    <ul>
+      <li v-for="item in menuItems">
+        {{item}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import RestaurantServices from '../services/RestaurantAPI.js'
+
+export default {
+  name: 'menu-items',
+  data() {
+    return {
+      menuItems: []
+    }
+  },
+  mounted() {
+    this.loadMenuItems();
+  },
+  methods: {
+    async loadMenuItems() {
+      const response = await RestaurantServices.getMenuItems();
+      this.menuItems = response.data;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
