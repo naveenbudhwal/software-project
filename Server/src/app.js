66e777960ecf35fa08a26770916aeda9c695dde5
naveenbudhwal <naveenbudhwal@gmail.com>
2019-11-09 23:54:11 +0530

    Connecting mongo database

--- Server/src/app.js
+++ Server/src/app.js
@@ -3,18 +3,47 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-const app = express() // create your express app
+const mongo = require('mongodb')
+
+const MongoClient = mongo.MongoClient
+const uri =
+  'mongodb+srv://default-user:aadjusting@cluster0-peiej.mongodb.net/Restaurant?retryWrites=true&w=majority'
+var client
+
+var mongoClient = new MongoClient(uri, {
+  reconnectTries: Number.MAX_VALUE,
+  autoReconnect: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+mongoClient.connect((err, db) => {
+  // returns db connection
+  if (err != null) {
+    console.log('Error occured while connecting to database', err)
+    return
+  }
+  client = db
+})
+
+const app = express()
 
 // make app use dependencies
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/todo', (req, res) => {
-  res.send([
-    'Thing 1',
-    'Thing 2'
-  ])
+app.get('/menuItems', (req, res) => {
+  const collection = client.db('Restaurant').collection('menu')
+  collection.find().toArray(function(err, results) {
+    if(err) {
+      console.log('Oops' + err)
+      res.send([])
+      return
+    }
+    
+    res.send(results)
+  })
 })
 
 
