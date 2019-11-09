ca2a875a3b7dd82591eab09274e18d5f2a651dbe
naveenbudhwal <naveenbudhwal@gmail.com>
2019-11-09 23:17:43 +0530

    Admin page

---
+++ Client/src/views/Admin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Admin page</h1>
+    <button @click="logout">Logout</button>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+  name: 'admin',
+  methods: {
+    logout: function() {
+      firebase.auth().signOut().then(() => {
+        this.$router.replace('login')
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
