3b4c735cf9db39032b9ff047f1a23264b4464c51
naveenbudhwal <naveenbudhwal@gmail.com>
2019-11-12 17:02:53 +0530

    More backend function calls

--- Client/src/services/RestaurantAPI.js
+++ Client/src/services/RestaurantAPI.js
@@ -3,5 +3,18 @@ import API from './API.js'
 export default {
   getMenuItems() {
     return API().get('menuItems')
+  },
+  addToCart(item) {
+    return API().post('addToCart', {
+      item: item
+    })
+  },
+  getCartItems() {
+    return API().get('cartItems')
+  },
+  deleteCartItem(item) {
+    return API().post('deleteCartItem', {
+      item: item
+    })
   }
 }
