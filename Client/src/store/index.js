e5d1ec262396a62ec5108ee5b63b199386afadd5
Naveen Budhwal <naveenbudhwal@gmail.com>
2020-05-01 13:53:00 +0530

    notification module

--- Client/src/store/index.js
+++ Client/src/store/index.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import * as menu from '@/store/modules/menu'
 import * as cart from '@/store/modules/cart'
+import * as notification from '@/store/modules/notification'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     menu,
-    cart
+    cart,
+    notification
   },
   state: {
    
